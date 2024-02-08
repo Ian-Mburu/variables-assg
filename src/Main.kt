f7f1c67ef3a3c362c33d4a7e6d02178802317dbf
ianongaruiya@gmail.com <ianongaruiya@gmail.com>
2024-02-08 13:47:03 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,25 @@
+fun main () {
+    var student1 = "Michael Jordan"
+    var age1 = "24"
+    var phoneNumber1 = 8787878765
+    var weight1 = "40kg"
+    var nationality1 = "Ugandan"
+
+    println(student1)
+    println(age1)
+    println(phoneNumber1)
+    println(weight1)
+    println(nationality1)
+
+    var student2 = "Ian Mburu"
+    var age2 = "26"
+    var phoneNumber2 = 7666554576
+    var weight2 = "50kg"
+    var nationality2 = "Kenyan"
+
+    println(student2)
+    println(age2)
+    println(phoneNumber2)
+    println(weight2)
+    println(nationality2)
+}
